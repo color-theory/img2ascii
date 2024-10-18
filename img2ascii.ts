9af9b1e96223b681c1eb22087e6e7bbab40464d6
Color <jgstringer@gmail.com>
2024-10-18 12:24:54 -0500

    simple image loading. only works with transparency

--- img2ascii.ts
+++ img2ascii.ts
@@ -1,29 +1,40 @@
 import * as fs from 'fs';
-import { createCanvas } from 'canvas';
+import { createCanvas, loadImage } from 'canvas';
+import * as path from 'path';
 
-const canvas = createCanvas(30, 18);
+const canvas = createCanvas(30, 17);
 const ctx = canvas.getContext('2d');
+const imgPath = path.join(__dirname, process.argv[2]);
 
 ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.font = '16px sans-serif';
-ctx.fillText("hi", 0, 15);
 
-const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-let visiblePixels = [];
+loadImage(imgPath).then((image) => {
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-for (let i = 0; i < pixelData.length; i += 4) {
-  let alpha = pixelData[i + 3];
-  visiblePixels.push(alpha > 0 ? 1 : 0);
-}
+  const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  let visiblePixels = [];
 
-let lineToPrint = '';
-let output = '\r\n';
-for (let i = 0; i < visiblePixels.length; i++) {
-  if (i % canvas.width === 0) {
-    output += lineToPrint + '\r\n';
-    lineToPrint = '';
+  for (let i = 0; i < pixelData.length; i += 4) {
+    let alpha = pixelData[i + 3];
+    visiblePixels.push(alpha > 0 ? 1 : 0);
   }
-  lineToPrint += visiblePixels[i] ? '⣿' : '⠀';
-}
-console.log(output);
-fs.writeFileSync('./output/output.txt', output);
+
+  let lineToPrint = '';
+  let output = '\r\n';
+  for (let i = 0; i < visiblePixels.length; i++) {
+    if (visiblePixels[i] === 1) {
+      console.log('i:', i);
+    }
+    if (i % canvas.width === 0) {
+      output += lineToPrint + '\r\n';
+      lineToPrint = '';
+    }
+    lineToPrint += visiblePixels[i] ? '⣿' : '⠀';
+  }
+  console.log(output);
+  fs.writeFileSync('./output/output.txt', output);
+}).catch((err) => {
+  console.error('Failed to load image:', err);
+});
+
+
